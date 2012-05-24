6032248149f368ca1193dcd449ad794b6b9ab64e
Luis Alberto Hernandez Monroy <lhernandez@kionetworks.com>
2012-05-24 13:28:44 -0500

    Se crea archivo con el formato JSON para cargar los datos

---
+++ js/cargaDatos.js
@@ -0,0 +1,27 @@
+//Variable que almacena el arreglo que se mostrara en el dropdown
+var ddData = [
+	{
+		text: "Facebook",
+		value: 1,
+		description: "Descripción Facebook",
+		imageSrc: "img/iconfacebook.png"
+	},
+	{
+		text: "Twitter",
+		value: 2,
+		description: "Descripción Twitter",
+		imageSrc: "img/icontwitter.png"
+	},
+	{
+		text: "LinkedIn",
+		value: 3,
+		description: "Descripción LinkedIn",
+		imageSrc: "img/iconlinkedin.png"
+	},
+	{
+		text: "Digg",
+		value: 4,
+		description: "Descripción Digg",
+		imageSrc: "img/icondigg.png"
+	}
+];
